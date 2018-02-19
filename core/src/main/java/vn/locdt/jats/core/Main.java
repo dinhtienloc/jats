a65f313f657142fce0ada83aa1d45c0f189b3cac
dinhtienloc <dinhtienloc1996@gmail.com>
2018-02-20 02:18:06 +0700

    modified Bootstrap.java file of spring-shell and change log level of all class in 'org.springframework' to OFF

--- core/src/main/java/vn/locdt/jats/core/Main.java
+++ core/src/main/java/vn/locdt/jats/core/Main.java
@@ -1,11 +1,11 @@
 package vn.locdt.jats.core;
 
-import org.springframework.shell.Bootstrap;
-import org.springframework.shell.core.JLineShell;
 import org.springframework.shell.event.AbstractShellStatusPublisher;
 import vn.locdt.jats.module.shell.listener.AppShellStatusListener;
 
+import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Created by locdt on 1/25/2018.
@@ -19,9 +19,21 @@ public class Main {
      */
     public static void main(String... args) throws Exception {
         LogManager.getLogManager().reset();
+
+        // Set a suitable priority level on Spring Framework log messages
+        Logger sfwLogger = Logger.getLogger("org.springframework");
+        sfwLogger.setLevel(Level.OFF);
+
+        // Set a suitable priority level on Roo log messages
+        // (see ROO-539 and HandlerUtils.getLogger(Class))
+//        Logger rooLogger = Logger.getLogger("org.springframework.shell");
+//        rooLogger.setLevel(Level.OFF);
+
+//        Bootstrap.main(args);
         Bootstrap bootstrap = new Bootstrap(args);
         AbstractShellStatusPublisher shellStatusPublisher = bootstrap.getJLineShellComponent();
         shellStatusPublisher.addShellStatusListener(new AppShellStatusListener());
         bootstrap.run();
     }
+
 }
